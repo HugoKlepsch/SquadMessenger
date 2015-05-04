944c7566926465744c1a479672fb866a8fc9a717
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-05-04 14:29:44 -0400

    Totally broke everything

--- app/src/main/java/hugra/squadmessenger/DisplayMessageActivity.java
+++ app/src/main/java/hugra/squadmessenger/DisplayMessageActivity.java
@@ -1,39 +1,23 @@
 package hugra.squadmessenger;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
-    public final static String EXTRA_MESSAGE = "com.hugra.squadmessenger.MESSAGE";
-
-    public void sendMessage(View v){
-//        System.out.println("Inside closeApp");
-//        Log.d("Swag", "Inside closeApp log.d");
-////        System.exit(0);
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.mainActivity_editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
+public class DisplayMessageActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_display_message);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_display_message, menu);
         return true;
     }
 
@@ -45,12 +29,10 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.appInfo) {
+        if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-
-
 }
