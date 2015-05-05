4dd237ad20d5a5973a543536db5644e7200d1763
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-05-04 21:26:29 -0400

    Added a second screen that displays a message that is passed to it. Text can be "enabled" or disabled.

--- app/src/main/java/hugra/squadmessenger/DisplayMessageActivity.java
+++ app/src/main/java/hugra/squadmessenger/DisplayMessageActivity.java
@@ -4,35 +4,27 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class DisplayMessageActivity extends ActionBarActivity {
+    private TextView swapButt;
+    private boolean isEnabledText = true;
+
+    public void swapEnabled(View v){
+        swapButt.setEnabled(!isEnabledText);
+        isEnabledText = !isEnabledText;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_display_message, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        swapButt = (TextView) findViewById(R.id.displayMessageLabel);
+        swapButt.setText(getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE));
+//        swapButt.setEnabled(true);
     }
 }
