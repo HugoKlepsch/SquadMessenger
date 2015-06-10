8abac0035c53ae83836567c99fb4612a2301fec0
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-06-10 19:44:26 -0400

    Commented AppInfo.java

--- app/src/main/java/hugra/squadmessenger/AppInfo.java
+++ app/src/main/java/hugra/squadmessenger/AppInfo.java
@@ -1,30 +1,32 @@
+/*
+		 Title: AppInfo.java
+		 Programmer: hugo
+		 Date of creation: June 1, 2015
+		 Description: The class/thread that describes the App info "activity" (window)
+ */
+
+
+
 package hugra.squadmessenger;
 
-import android.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.TextView;
 
 
 public class AppInfo extends AppCompatActivity {
-    private final int leftJust = Gravity.START;
-    private final int centerJust = Gravity.CENTER;
-    private final int rightJust = Gravity.END;
-    TextView welcomeText;
-    private int gravState;
-
-
-
-
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app_info);
-        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+    protected void onCreate(Bundle savedInstanceState) { //The only thing this activity has to do
+    // is create itself, load the layout from xml & load the message
+        super.onCreate(savedInstanceState); //If there is a saved instance of the window, use
+        // that (this is there by deafult)
+        setContentView(R.layout.activity_app_info); //load the layout from the xml file
+
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar(); //the following lines
+        // gets the title bar that includes the back/up button, and removes it because it causes
+        // the mainActivity to reconnect to the server in stead of keeping it in the background.
         actionBar.setDisplayHomeAsUpEnabled(false); // remove the left caret
         if (actionBar != null) {
             actionBar.setHomeButtonEnabled(false); // disable the button
