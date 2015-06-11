b167c5fd518112d2373e85b40f53b6438422fce5
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-06-10 21:38:19 -0400

    Commented showusers and changed showusers size to be more visible

--- app/src/main/java/hugra/squadmessenger/ShowUsers.java
+++ app/src/main/java/hugra/squadmessenger/ShowUsers.java
@@ -18,20 +18,23 @@ import java.util.Vector;
 public class ShowUsers extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Vector<String> userList = MainActivity.users;
+    protected void onCreate(Bundle savedInstanceState) { //runs on activity open
+        Vector<String> userList = MainActivity.users; //holds the list of users
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_show_users);
-        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        setContentView(R.layout.activity_show_users); //load the layout from the xml file
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar(); //the following lines
+        // gets the title bar that includes the back/up button, and removes it because it causes
+        // the mainActivity to reconnect to the server in stead of keeping it in the background.
         actionBar.setDisplayHomeAsUpEnabled(false); // remove the left caret
         if (actionBar != null) {
             actionBar.setHomeButtonEnabled(false); // disable the button
 
             actionBar.setDisplayShowHomeEnabled(false); // remove the icon
         }
+
         TextView users = (TextView) findViewById(R.id.ShowUsers_Users);
-        users.setText("Users: \n");
+        users.setText("Users: \n"); //add each user to the screen
         for (int i = 0; i < userList.size(); i ++){
             users.setText(users.getText() + userList.get(i) + "\n");
         }
